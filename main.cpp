b200114e9897286608577c7265b57dbc596e82b8
Ілля <yavd3341@gmail.com>
2019-10-03 22:24:54 +0300

    Fox fires - Beta 4

--- main.cpp
+++ main.cpp
@@ -4,6 +4,29 @@
 #include <cmath>
 #include <stdio.h>
 
+// DRAW (MAIN) FLAGS
+#define FLAG_NODRAW 0
+#define FLAG_FULLSCREEN 1
+#define FLAG_FADE_OUT 2
+#define FLAG_PAUSE 3
+
+// DATA FLAGS
+#define FLAG_UPDATE_DATA 4
+#define FLAG_CYCLE_DATA 5
+
+// Y SINE FLAGS
+#define FLAG_USE_Y_SINE 6
+#define FLAG_UPDATE_Y_SINE 7
+#define FLAG_USE_Y_SINE_WANE 8
+
+// COLOR FLAGS
+#define FLAG_MONOCHROME 9
+#define FLAG_UPDATE_COLOR 10
+
+// WANE SINE FLAGS
+#define FLAG_USE_WANE_SINE 11
+#define FLAG_UPDATE_WANE_SINE 12
+
 unsigned int w = 800;
 unsigned int h = 600;
 
@@ -20,18 +43,7 @@ double waneSineDelta = 2.5;
 double colorDelta = 0.001;
 double colorDeltaManual = colorDelta * 10;
 
-/*
-	0 - pause
-	1 - fade
-	2 - no draw
-	3 - update sine
-	4 - update color
-	5 - update wane
-	6 - use wane
-	7 - fullscreen
-*/
-
-bool controls[] = {false, false, false, true, true, true, true, false};
+bool flags[13];
 
 int dataLength = sf::VideoMode::getDesktopMode().width;
 char* data = new char[dataLength];
@@ -56,7 +68,6 @@ sf::Color getMask(double val){
 }
 
 void draw(sf::RenderWindow * window){
-	
 	window->clear(backColor);
 
 	for(int i = 0; i < w; i++)
@@ -65,22 +76,23 @@ void draw(sf::RenderWindow * window){
 				
 		double c = (currentData & 0xFF) / 255.0;
 		double mapI = map(i, 0, w, 0, 180);
-		double s = controls[1] ? fabs(sin(mapI * M_PI / 180.0)) : 1;
+		double maskDouble = flags[FLAG_FADE_OUT] ? fabs(sin(mapI * M_PI / 180.0)) : 1;
+		double yMod = flags[FLAG_USE_Y_SINE] ? map(sin((ySineOffset + mapI * 2) * M_PI / 180.0), -1, 1, 0, 1) : 0;
+		
+		if(flags[FLAG_USE_Y_SINE_WANE])
+			maskDouble *= map(yMod, 0, 1, 1, 0.8);
 
-		double ySine = sin((ySineOffset + mapI * 2) * M_PI / 180.0);
-		int yOffset = map(ySine, -1, 1, 0, h * (1 - size));
-
-		if(controls[6])
+		if(flags[FLAG_USE_WANE_SINE])
 		{
 			double waneSine = sin((waneSineOffset + mapI * 4) * M_PI / 180.0);
-			double waneStage = map(waneSine, -1, 1, 0.8, 1);
-			s *= waneStage;
+			double waneStage = map(waneSine, -1, 1, flags[FLAG_USE_Y_SINE_WANE] && flags[FLAG_USE_Y_SINE] ? 0.9: 0.8, 1);
+			maskDouble *= waneStage;
 		}
 
-		double oneI = map(i, 0, w, 0, 1);
+		double oneI = flags[FLAG_MONOCHROME] ? 0.5 : map(i, 0, w, 0, 1);
 		double x1 = colorOffset;
 		int colorBaseIndex = (int) x1;
-
+		
 		x1 -= colorBaseIndex;
 		x1 += oneI;
 
@@ -91,24 +103,25 @@ void draw(sf::RenderWindow * window){
 		}
 
 		double x2 = 1 - x1;
+		
+		sf::Color color = colors[colorBaseIndex % colorsLength];
+		sf::Color mixedColor = colors[(colorBaseIndex + 1) % colorsLength];
 
-		sf::Color color1 = colors[colorBaseIndex % colorsLength];
-		sf::Color color2 = colors[(colorBaseIndex + 1) % colorsLength];
+		sf::Color mask1 = getMask(x2); 
+		sf::Color mask2 = getMask(x1);
 
-		sf::Color mask1 = getMask(x1); 
-		sf::Color mask2 = getMask(x2);
-
-		sf::Color color = color1 * mask2 + color2 * mask1;
-		sf::Color mask(s * 255, s * 255, s * 255, s * 255);
+		color = color * mask1 + mixedColor * mask2;
+		
+		sf::Color mask(maskDouble * 255, maskDouble * 255, maskDouble * 255, maskDouble * 255);
 
 		if (w-i < w)
 		{
 			sf::Vertex line[] =
 			{
-				sf::Vertex(sf::Vector2f(w-i, yOffset + map(h-(h * c), 0, h, 0, h * size)), backColor),
-				sf::Vertex(sf::Vector2f(w-i, yOffset + map(h-bottomMargin-coreHeight, 0, h, 0, h * size)), color * mask),
-				sf::Vertex(sf::Vector2f(w-i, yOffset + map(h-bottomMargin, 0, h, 0, h * size)), color * mask),
-				sf::Vertex(sf::Vector2f(w-i, yOffset + map(h-(bottomMargin-(bottomMargin * c)), 0, h, 0, h * size)), backColor)
+				sf::Vertex(sf::Vector2f(w-i, (yMod * h * (1 - size)) + map(h-(h * c), 0, h, 0, h * size)), backColor),
+				sf::Vertex(sf::Vector2f(w-i, (yMod * h * (1 - size)) + map(h-bottomMargin-coreHeight, 0, h, 0, h * size)), color * mask),
+				sf::Vertex(sf::Vector2f(w-i, (yMod * h * (1 - size)) + map(h-bottomMargin, 0, h, 0, h * size)), color * mask),
+				sf::Vertex(sf::Vector2f(w-i, (yMod * h * (1 - size)) + map(h-(bottomMargin-(bottomMargin * c)), 0, h, 0, h * size)), backColor)
 			};
 
 			window->draw(line, 4, sf::LineStrip);
@@ -118,13 +131,36 @@ void draw(sf::RenderWindow * window){
 
 int main()
 {
+	// Setting flags
+	
+	flags[FLAG_NODRAW] = false;
+	flags[FLAG_FULLSCREEN] = false;
+	flags[FLAG_FADE_OUT] = false;
+	flags[FLAG_PAUSE] = false;
+
+	flags[FLAG_UPDATE_DATA] = true;
+	flags[FLAG_CYCLE_DATA] = false;
+
+	flags[FLAG_USE_Y_SINE] = true;
+	flags[FLAG_UPDATE_Y_SINE] = true;
+	flags[FLAG_USE_Y_SINE_WANE] = true;
+
+	flags[FLAG_MONOCHROME] = false;
+	flags[FLAG_UPDATE_COLOR] = true;
+
+	flags[FLAG_USE_WANE_SINE] = true;
+	flags[FLAG_UPDATE_WANE_SINE] = true;
+	
+	// Continue init
+	
 	int fps = 60;
 	sf::ContextSettings settings;
 	settings.antialiasingLevel = 8;
 	
-	sf::RenderWindow window(controls[7] ? sf::VideoMode::getDesktopMode() : sf::VideoMode(w, h), "Fox Fires", controls[7] ? sf::Style::Fullscreen : sf::Style::Default, settings);
+	sf::RenderWindow window(flags[FLAG_FULLSCREEN] ? sf::VideoMode::getDesktopMode() : sf::VideoMode(w, h),
+		"Fox Fires", flags[FLAG_FULLSCREEN] ? sf::Style::Fullscreen : sf::Style::Default, settings);
 	window.setFramerateLimit(fps);
-	window.setMouseCursorVisible(!controls[7]);
+	window.setMouseCursorVisible(!flags[FLAG_FULLSCREEN]);
 	
 	srand(time(NULL));
 	
@@ -167,32 +203,53 @@ int main()
 				}
 				
 				if (event.key.code == sf::Keyboard::Space)
-					controls[0] = !controls[0];
+					flags[FLAG_PAUSE] = !flags[FLAG_PAUSE];
 				
-				if (event.key.code == sf::Keyboard::F)
-					controls[1] = !controls[1];
+				if (event.key.code == sf::Keyboard::D)
+				{
+					if(sf::Keyboard::isKeyPressed(sf::Keyboard::LShift) || sf::Keyboard::isKeyPressed(sf::Keyboard::RShift))
+						flags[FLAG_CYCLE_DATA] = !flags[FLAG_CYCLE_DATA];
+					else
+						flags[FLAG_UPDATE_DATA] = !flags[FLAG_UPDATE_DATA];
+				}
+				
+				if (event.key.code == sf::Keyboard::F || event.key.code == sf::Keyboard::F11)
+				{
+					flags[FLAG_FULLSCREEN] = !flags[FLAG_FULLSCREEN];
+					window.create(flags[FLAG_FULLSCREEN] ? sf::VideoMode::getDesktopMode() : sf::VideoMode(w / 2, h / 2),
+						"Fox Fires", flags[FLAG_FULLSCREEN] ? sf::Style::Fullscreen : sf::Style::Default, settings);
+					window.setFramerateLimit(fps);
+					window.setMouseCursorVisible(!flags[FLAG_FULLSCREEN]);
+				}
 				
 				if (event.key.code == sf::Keyboard::S)
-					controls[3] = !controls[3];
+				{
+					if(sf::Keyboard::isKeyPressed(sf::Keyboard::LShift) || sf::Keyboard::isKeyPressed(sf::Keyboard::RShift))
+						flags[FLAG_USE_Y_SINE] = !flags[FLAG_USE_Y_SINE];
+					else if(sf::Keyboard::isKeyPressed(sf::Keyboard::LControl) || sf::Keyboard::isKeyPressed(sf::Keyboard::RControl))
+						flags[FLAG_USE_Y_SINE_WANE] = !flags[FLAG_USE_Y_SINE_WANE];
+					else
+						flags[FLAG_UPDATE_Y_SINE] = !flags[FLAG_UPDATE_Y_SINE];
+				}
+				
+				if (event.key.code == sf::Keyboard::O)
+					flags[FLAG_FADE_OUT] = !flags[FLAG_FADE_OUT];
 				
 				if (event.key.code == sf::Keyboard::C)
-					controls[4] = !controls[4];
+				{
+					if(sf::Keyboard::isKeyPressed(sf::Keyboard::LShift) || sf::Keyboard::isKeyPressed(sf::Keyboard::RShift))
+						flags[FLAG_MONOCHROME] = !flags[FLAG_MONOCHROME];
+					else
+						flags[FLAG_UPDATE_COLOR] = !flags[FLAG_UPDATE_COLOR];
+				}
 				
 				if (event.key.code == sf::Keyboard::W)
 				{
 					if(sf::Keyboard::isKeyPressed(sf::Keyboard::LShift) || sf::Keyboard::isKeyPressed(sf::Keyboard::RShift))
-						controls[6] = !controls[6];
+						flags[FLAG_USE_WANE_SINE] = !flags[FLAG_USE_WANE_SINE];
 					else
-						controls[5] = !controls[5];
-				}
-				
-				if (event.key.code == sf::Keyboard::M)
-				{
-					controls[7] = !controls[7];
-					window.create(controls[7] ? sf::VideoMode::getDesktopMode() : sf::VideoMode(w / 2, h / 2), "Fox Fires", controls[7] ? sf::Style::Fullscreen : sf::Style::Default, settings);
-					window.setFramerateLimit(fps);
-					window.setMouseCursorVisible(!controls[7]);
-				}
+						flags[FLAG_UPDATE_WANE_SINE] = !flags[FLAG_UPDATE_WANE_SINE];
+				}				
 			}
 					
 			if (event.type == sf::Event::Resized)
@@ -205,24 +262,28 @@ int main()
 		w = window.getSize().x;
 		h = window.getSize().y;
 		
-		if(!controls[2])
+		if(!flags[FLAG_NODRAW])
 			draw(&window);
 
-		if(!controls[0]){
+		if(flags[FLAG_UPDATE_DATA] && !flags[FLAG_PAUSE]){
+			int firstData = data[0];
 			for(int i = 0; i < dataLength; i++)
 				if (i > 0)
 					data[i - 1] = data[i];
-				
-			data[dataLength - 1] = 128 + rand() % 128;
+			
+			if(flags[FLAG_CYCLE_DATA])
+				data[dataLength - 1] = firstData;
+			else
+				data[dataLength - 1] = 128 + rand() % 128;
 		}
 		
-		if(controls[3])
+		if(flags[FLAG_UPDATE_Y_SINE] && flags[FLAG_USE_Y_SINE] && !flags[FLAG_PAUSE])
 			ySineOffset += ySineDelta;
 		
-		if(controls[4])
+		if(flags[FLAG_UPDATE_COLOR] && !flags[FLAG_MONOCHROME] && !flags[FLAG_PAUSE])
 			colorOffset += colorDelta;
 		
-		if(controls[5])
+		if(flags[FLAG_UPDATE_WANE_SINE] && flags[FLAG_USE_WANE_SINE] && !flags[FLAG_PAUSE])
 			waneSineOffset += waneSineDelta;
 		
 		if(ySineOffset > 360)
